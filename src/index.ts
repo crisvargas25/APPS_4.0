99a4fbacf5e14519a47fd72b9e5ee360928e800b
crisvargas25 <cristianarturo022@gmail.com>
2025-07-17 11:23:00 -0600

    cors ?

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Permitir todas las solicitudes CORS
-app.use(cors({
-  origin: '*', // ¡Permitir todos los orígenes!
-  credentials: true,
-}));
+app.use(cors());
+
 
 app.use(express.json());
 app.use(morgan('dev'));
